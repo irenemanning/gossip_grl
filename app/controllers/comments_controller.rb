d24280593798b5a178f46ce9283d573b3df717d7
Irene <irenemanningm@gmail.com>
2023-10-11 17:37:20 -0500

    Add controller actions to comments and posts.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,29 @@
 class CommentsController < ApplicationController
+    before_action :authorize
+    def index
+        comments = Comment.all
+        render json: comments
+    end
+    def show
+        comment = Comment.find_by(id: params[:id])
+        render json: comment
+    end
+    def create 
+        comment = @current_user.comments.create!(comment_params)
+        render json: comment, status: :created
+    end
+    def destroy
+        comment = find_comment
+        comment.destroy
+        head :no_content
+    end
 
+    private
+
+    def comment_params
+        params.permit(:body, :post_id)
+    end
+    def find_comment
+        @current_user.comments.find_by(params[:id])
+    end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,34 @@
 class PostsController < ApplicationController
-    
+    before_action :authorize
     def index
+        posts = Post.all
+        render json: posts
     end
     def show
+        post = find_post
+        render json: wine
     end
-    def post
+    def create
+        post = Post.create!(post_params)
+        render json: post, status: :created
     end
     def update
+        post = find_post
+        post.update(post_params)
+        render json: post
     end
     def destroy
+        post = find_post
+        post.destroy
+        head :no_content
+    end
+
+    private 
+
+    def post_params
+        params.permit(:body)
+    end
+    def find_post
+        Post.find_by(id: params[:id])
     end
 end
