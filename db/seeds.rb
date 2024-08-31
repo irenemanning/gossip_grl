8a861e14518570c7453de8df2364f1e3480aade9
Irene <irenemanningm@gmail.com>
2024-08-31 02:54:06 -0500

    Add demo user seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 
-
+demo = User.create(username: 'demo', password: 'demo')
 user1 = User.create(username: 'user1', password: 'password1')
 user2 = User.create(username: 'user2', password: 'password2')
 
