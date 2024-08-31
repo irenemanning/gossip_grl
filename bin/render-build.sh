cc2875b1dc0fc681d8a1c35f32a96fe52101bcf6
Irene <irenemanningm@gmail.com>
2024-08-31 03:04:28 -0500

    Uncomment db:seed in render-build file.

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -11,5 +11,5 @@ cp -a client/build/. public/
 bundle install
 bundle exec rake db:migrate 
 
-# bundle exec rake db:seed 
+bundle exec rake db:seed 
 # if you have seed data, run this command for the initial deploy only to avoid duplicate records
